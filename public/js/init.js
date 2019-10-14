d076f8afc74dea89b46f159ecf8f7a80c424dcb6
Ritom Sonowal <ritomsonowal99@gmail.com>
2019-10-14 15:57:27 +0530

    Add Firebase Ref

---
+++ public/js/init.js
@@ -0,0 +1,9 @@
+jQuery(document).ready( function( $ ) {
+
+    // Firebase
+    var ref = new Firebase('https://project-banana-4822.firebaseio.com/');
+
+    console.log('Firebase connection established!');
+
+
+});
