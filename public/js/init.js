045b5aaf98b065be6b405c47def2b6aa5e31bcfd
Ritom Sonowal <ritomsonowal99@gmail.com>
2019-10-14 23:55:55 +0530

    Complete Submit Ideas

--- public/js/init.js
+++ public/js/init.js
@@ -1,6 +1,6 @@
 jQuery(document).ready( function( $ ) {
 
-     // Your web app's Firebase configuration
+     // Firebase configuration
     var firebaseConfig = {
         apiKey: "AIzaSyB1mDD2MLmghXbxZ4YSTK61KfYKrw2xAtQ",
         authDomain: "project-banana-4822.firebaseapp.com",
@@ -8,7 +8,7 @@ jQuery(document).ready( function( $ ) {
         projectId: "project-banana-4822",
         storageBucket: "project-banana-4822.appspot.com",
         messagingSenderId: "120654780626",
-       appId: "1:120654780626:web:bb5a034a16b3ad10da350b",
+        appId: "1:120654780626:web:bb5a034a16b3ad10da350b",
         measurementId: "G-92NW0Q5WD0"
     };
     
@@ -19,35 +19,37 @@ jQuery(document).ready( function( $ ) {
     // Get a reference to the database service
     var database = firebase.database();
 
-    function saveData() {
+        $("form").submit(function(){
 
-        // get data
-            var Contact = "https://github.com/dsc-nitr/";
-            var Date = "2019-10-14";
-            var Desc = "Interactive Map for NITR";
-            var Name = "Abel Mathew";
-            var Stack = "App Developement, Flutter";
-            var Title = "NITR Route"
+            // console.log('Aaa');
+            var Contact = document.getElementById('contact').value;
+            var Desc = document.getElementById('desc').value;
+            var Name = document.getElementById('name').value;
+            var Stack = document.getElementById('stack').value;
+            var Title = document.getElementById('title').value;
 
-        firebase.database().ref('ideas/' + 'nitr-route').set({
+            var today = new Date();
+            var now = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+            var id = Title.toLowerCase().replace(/ /g, "-");
+
+            id = id + ':' + now.toString();
+
+        firebase.database().ref('ideas/' + id).set({
             contact: Contact,
-            date: Date,
+            date: now,
             desc: Desc,
             name: Name,
             stack: Stack,
             title: Title
           }, function(error) {
             if (error) {
-              // The write failed...
               console.log('Could not save data...');
             } else {
-              // Data saved successfully!
               console.log('Write Successfull...');
             }
           });
-    }
-        
-
-    var x = saveData();
+        });
+    
 
 });
